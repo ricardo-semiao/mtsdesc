11d7db808f75d6e14e35c2c720ee92619641b227
ricardo-semiao <ricardo.semiao@outlook.com>
2024-07-22 16:32:16 -0300

    Updated dependencies

--- R/varr-package.R
+++ R/varr-package.R
@@ -2,13 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import rlang
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 vars
+#' @importFrom glue glue
 #' @importFrom magrittr %>%
-#' @importFrom rlang !!!
-#' @importFrom rlang %||%
-#' @importFrom rlang .data
-#' @importFrom rlang inject
 ## usethis namespace: end
 NULL
